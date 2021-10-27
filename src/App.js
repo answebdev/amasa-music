95befb6c94bb178f859c8643af9180685bf7b98a
ANS <36783010+answebdev@users.noreply.github.com>
2021-10-26 21:34:26 -0700

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,86 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [artists, setArtists] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('Coldplay');
+
+  useEffect(() => {
+    fetchArtists();
+  }, []);
+
+  // Fetch data
+  const fetchArtists = async () => {
+    // const url = `https://theaudiodb.com/api/v1/json/1/search.php?s=${searchTerm}`;
+    const url = `https://theaudiodb.com/api/v1/json/1/search.php?s=coldplay`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    console.log(data);
+    // console.log(searchTerm);
+
+    if (data.artists) {
+      setArtists(data.artists);
+    }
+  };
+
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
         <p>AMASA Music</p>
+
+        <div>
+          {artists.map((artist) => {
+            return (
+              <div key={artist.idArtist}>
+                <p>Artist: {artist.strArtist}</p>
+                <p>Country: {artist.strCountry}</p>
+                <p>Genre: {artist.strGenre}</p>
+                <p>
+                  Website:{' '}
+                  <a
+                    href={`https://${artist.strWebsite}`}
+                    rel='noopener noreferrer'
+                    target='_blank'
+                  >
+                    {artist.strArtist}
+                  </a>
+                </p>
+                <p>
+                  Twitter:{' '}
+                  <a
+                    href={`https://${artist.strTwitter}`}
+                    rel='noopener noreferrer'
+                    target='_blank'
+                  >
+                    {artist.strArtist}
+                  </a>
+                </p>
+              </div>
+            );
+          })}
+          {/* <p>{searchTerm}</p> */}
+        </div>
+
+        {/* {artists.map((artist) => {
+          return (
+            <div>
+              <p>{artist.strArtist}</p>;
+            </div>
+          );
+        })} */}
       </header>
     </div>
   );
-}
+};
 
 export default App;
