10f1c2072339185d008da3bd2d41957cce4f6842
ANS <36783010+answebdev@users.noreply.github.com>
2021-10-28 12:03:54 -0700

    style album page

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-// import classes from '../styles/Artist.module.css';
-// import axios from 'axios';
+import { Row, Col, Card, CardGroup, Accordion } from 'react-bootstrap';
+import classes from '../styles/Artist.module.css';
 
 const Artist = ({ match }) => {
-  //   const [data, setData] = useState([]);
   const [album, setAlbums] = useState([]);
 
   const history = useHistory();
@@ -38,6 +37,12 @@ const Artist = ({ match }) => {
     }
   };
 
+  const artistStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '2rem',
+  };
+
   return (
     <div>
       {album.map((item) => (
@@ -46,59 +51,156 @@ const Artist = ({ match }) => {
         </Helmet>
       ))}
 
-      <div style={{ marginTop: '40px', marginBottom: '40px' }}>
-        <Link to='/' variant='dark' role='button' aria-pressed='true'>
-          <i className='fas fa-arrow-left' onClick={() => history.goBack()}></i>
-          &nbsp;BACK TO ARTISTS
-        </Link>
-      </div>
-      <div>
-        {album.map((item) => {
-          return (
-            <div key={item.id}>
-              <img
-                style={{ width: '25%', height: '25%' }}
-                src={item.strAlbumThumb}
-                alt={item.strArtist}
-              />
-              <p>Artist: {item.strArtist}</p>
-              <p>Album: {item.strAlbum}</p>
-              <p>Year: {item.intYearReleased}</p>
-              <p>Genre: {item.strGenre}</p>
-              {/* <p>Review: {item.strReview}</p> */}
-            </div>
-          );
-        })}
+      <div className={classes.OuterContainer}>
+        <div className={classes.Container}>
+          <div style={artistStyle}>
+            {album.map((item) => {
+              return (
+                <Row key={item.id}>
+                  <Col>
+                    {/* {album ? ( */}
+                    <Card className={classes.MainCard}>
+                      <Card.Img
+                        variant='top'
+                        src={item.strAlbumThumb}
+                        alt={item.strArtist}
+                      />
+                      <Card.Body>
+                        <Card.Title>{item.strAlbum}</Card.Title>
+                        <Card.Text>
+                          <strong>Year: </strong>
+                          {item.intYearReleased}
+                          <br />
+                          <strong>Genre: </strong>
+                          {item.strGenre}
+                          <br />
+                          <strong>Label: </strong>
+                          {item.strLabel}
+                        </Card.Text>
 
-        <div style={{ marginTop: '40px', marginBottom: '40px' }}>
-          <Link to='/' variant='dark' role='button' aria-pressed='true'>
-            <i
-              className='fas fa-arrow-left'
-              onClick={() => history.goBack()}
-            ></i>
-            &nbsp;BACK TO ARTISTS
-          </Link>
-        </div>
-        {/* {data.map((item) => (
-          <div key={item.id}>
-            <div>
-              <p>
-                {item.album} ({item.year})
-              </p>
-
-              <p>Track Listing</p>
-
-              {item.tracks.map((track, index) => (
-                <div key={index}>
-                  <p>{track}</p>
-                </div>
-              ))}
-            </div>
+                        <Card.Text>
+                          <Accordion defaultActiveKey='1' flush>
+                            <Accordion.Item eventKey='0'>
+                              <Accordion.Header>Description</Accordion.Header>
+                              <Accordion.Body>
+                                {item.strDescriptionEN}
+                              </Accordion.Body>
+                            </Accordion.Item>
+                          </Accordion>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                    {/* ) : null} */}
+                  </Col>
+                </Row>
+              );
+            })}
           </div>
-        ))} */}
+
+          <div style={{ marginTop: '40px', marginBottom: '40px' }}>
+            <Link
+              className={classes.Link}
+              to='/'
+              variant='dark'
+              role='button'
+              aria-pressed='true'
+            >
+              <i
+                className='fas fa-arrow-left'
+                onClick={() => history.goBack()}
+              ></i>
+              &nbsp;Search
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Artist;
+
+// import React, { useState, useEffect } from 'react';
+// import { Link, useHistory } from 'react-router-dom';
+// import { Helmet } from 'react-helmet';
+// // import classes from '../styles/Artist.module.css';
+
+// const Artist = ({ match }) => {
+//   const [album, setAlbums] = useState([]);
+
+//   const history = useHistory();
+
+//   useEffect(() => {
+//     fetchAlbums();
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, []);
+
+//   const fetchAlbums = async () => {
+//     const albumUrl = `https://theaudiodb.com/api/v1/json/${1}/album.php?i=${
+//       match.params.id
+//     }`;
+
+//     const response = await fetch(albumUrl);
+//     const data = await response.json();
+
+//     fetch(albumUrl)
+//       .then((response) => {
+//         return response.json();
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+
+//     console.log(data);
+
+//     if (data.album) {
+//       setAlbums(data.album);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       {album.map((item) => (
+//         <Helmet>
+//           <title key={item.id}>Amasa Music | Albums | {item.strArtist}</title>
+//         </Helmet>
+//       ))}
+
+//       <div style={{ marginTop: '40px', marginBottom: '40px' }}>
+//         <Link to='/' variant='dark' role='button' aria-pressed='true'>
+//           <i className='fas fa-arrow-left' onClick={() => history.goBack()}></i>
+//           &nbsp;BACK TO ARTISTS
+//         </Link>
+//       </div>
+//       <div>
+//         {album.map((item) => {
+//           return (
+//             <div key={item.id}>
+//               <img
+//                 style={{ width: '25%', height: '25%' }}
+//                 src={item.strAlbumThumb}
+//                 alt={item.strArtist}
+//               />
+//               <p>Artist: {item.strArtist}</p>
+//               <p>Album: {item.strAlbum}</p>
+//               <p>Year: {item.intYearReleased}</p>
+//               <p>Genre: {item.strGenre}</p>
+//             </div>
+//           );
+//         })}
+
+//         <div style={{ marginTop: '40px', marginBottom: '40px' }}>
+//           <Link to='/' variant='dark' role='button' aria-pressed='true'>
+//             <i
+//               className='fas fa-arrow-left'
+//               onClick={() => history.goBack()}
+//             ></i>
+//             &nbsp;BACK TO ARTISTS
+//           </Link>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Artist;
--- src/components/ArtistResults.js
+++ src/components/ArtistResults.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import Spinner from './misc/Spinner';
 import classes from '../styles/ArtistResults.module.css';
-import { Row, Col, Image, Card, Button } from 'react-bootstrap';
+import { Row, Col, Card } from 'react-bootstrap';
 
 const ArtistResults = (props) => {
   const [error] = useState(false);
@@ -18,7 +18,6 @@ const ArtistResults = (props) => {
         ></link>
       </Helmet>
 
-      {/* <div> */}
       {props.artists.map((artist) => {
         return (
           <div className={classes.OuterContainer}>
@@ -80,94 +79,8 @@ const ArtistResults = (props) => {
           <p style={{ textAlign: 'center' }}>Oh, no. something went wrong!</p>
         </div>
       ) : null}
-      {/* </div> */}
     </div>
   );
 };
 
 export default ArtistResults;
-
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import { Helmet } from 'react-helmet';
-// import Spinner from './misc/Spinner';
-// import classes from '../styles/ArtistResults.module.css';
-// import { Row, Col, Card, Button } from 'react-bootstrap';
-
-// const ArtistResults = (props) => {
-//   const [error] = useState(false);
-
-//   return (
-//     <div className={classes.Container}>
-//       <Helmet>
-//         <title>Amasa Music | Search Artists</title>
-//         <link
-//           rel='stylesheet'
-//           href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
-//         ></link>
-//       </Helmet>
-//       <div>
-//         {props.artists.map((artist) => {
-//           return (
-//             <Row key={artist.idArtist}>
-//               <Col md={6}>
-//                 <div>
-//                   <Card style={{ width: '28rem', marginBottom: '40px' }}>
-//                     <Card.Img
-//                       variant='top'
-//                       src={artist.strArtistWideThumb}
-//                       alt={artist.strArtist}
-//                     />
-//                     <Card.Body>
-//                       <Card.Title>{artist.strArtist}</Card.Title>
-//                       <Card.Text>Country: {artist.strCountry}</Card.Text>
-//                       <Card.Text>Genre: {artist.strGenre}</Card.Text>
-//                       <Card.Text>
-//                         {' '}
-//                         Website:{' '}
-//                         <a
-//                           href={`https://${artist.strWebsite}`}
-//                           rel='noopener noreferrer'
-//                           target='_blank'
-//                         >
-//                           {artist.strArtist}
-//                         </a>
-//                       </Card.Text>
-//                       <Card.Text>
-//                         {' '}
-//                         Twitter:{' '}
-//                         <a
-//                           href={`https://${artist.strTwitter}`}
-//                           rel='noopener noreferrer'
-//                           target='_blank'
-//                         >
-//                           {artist.strArtist}
-//                         </a>
-//                       </Card.Text>
-// <Link
-//   className={classes.Link}
-//   to={`artist/${artist.idArtist}`}
-// >
-//   Albums
-// </Link>
-//                     </Card.Body>
-//                   </Card>
-//                 </div>
-//               </Col>
-//             </Row>
-//           );
-//         })}
-
-//         {!props.isLoading ? <div className='text-center'></div> : <Spinner />}
-
-//         {!props.isLoading && error ? (
-//           <div>
-//             <p style={{ textAlign: 'center' }}>Oh, no. something went wrong!</p>
-//           </div>
-//         ) : null}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ArtistResults;
